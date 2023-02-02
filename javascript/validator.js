25cd1e1bf5905b5dfb1e51081b5c8174f78ad031
‪Ahmed Aly Mohamed <31140616+ahmedali201720@users.noreply.github.com>
2023-02-02 14:23:46 +0200

    Adding messages and validators to login page

---
+++ javascript/validator.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+  // Fix validator bug for email addresses.
+  jQuery.validator.addMethod(
+    "email",
+    function (value, element) {
+      return (
+        this.optional(element) ||
+        /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i.test(
+          value
+        )
+      );
+    },
+    "Please enter valid email address!"
+  );
+  // Validate Login Form
+  $("#loginForm").validate({
+    rules: {
+      email: {
+        required: true,
+        email: true,
+      },
+      password: "required",
+    },
+    messages: {
+      password: "Please Enter your password",
+      email: {
+        required: "Email address is required",
+        email: "Enter a valid Email Address",
+      },
+    },
+    errorPlacement: (error, element) => {
+      element.parent().after(error);
+      element.parent().addClass("has-error");
+    },
+    success: (error, element) => {
+      $(element).parent().removeClass("has-error");
+      error.remove();
+    },
+    onsubmit: () => {},
+  });
+});
